180dfb156e9f6682bb3748da46c1407e5527f237
Nguyenx2 <dnguyenx688@gmail.com>
2024-06-11 23:56:00 +0700

    refactor(auth): remove instropect functionality and IntrospectResponse.java, IntrospectRequest.java. - Remove instrospect endpoint and related logic from AuthenticationController. - Deleted IntrospectRequest class, IntrospectResponse class as it is no longer needed.

--- src/main/java/com/example/xemtruyen/controllers/AuthenticationController.java
+++ src/main/java/com/example/xemtruyen/controllers/AuthenticationController.java
@@ -1,19 +1,14 @@
 package com.example.xemtruyen.controllers;
 
 import com.example.xemtruyen.dtos.auth.AuthenticationDTO;
-import com.example.xemtruyen.dtos.auth.IntrospectRequest;
 import com.example.xemtruyen.reponses.ApiResponse;
 import com.example.xemtruyen.reponses.auth.AuthenticationResponse;
-import com.example.xemtruyen.reponses.auth.IntrospectResponse;
 import com.example.xemtruyen.services.auth.AuthenticationService;
-import com.nimbusds.jose.JOSEException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.text.ParseException;
 
 import static com.example.xemtruyen.constant.Constant.MessageException.LOGIN_SUCCESSFULLY;
 
@@ -28,14 +23,6 @@ public class AuthenticationController {
         return ApiResponse.ofSuccess(
                 LOGIN_SUCCESSFULLY,
                 authenticationService.authenticate(request)
-        );
-    }
-
-    @PostMapping("/introspect")
-    ApiResponse<IntrospectResponse> authenticate(@RequestBody IntrospectRequest request) throws ParseException, JOSEException {
-        return ApiResponse.ofSuccess(
-                LOGIN_SUCCESSFULLY,
-                authenticationService.introspect(request)
         );
     }
 }
